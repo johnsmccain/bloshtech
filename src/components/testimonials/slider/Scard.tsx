0fe6be591c4d4ddac428ac7e0ac8e6aa6684d1c7
John Danlami <johnsdanlami@gmail.com>
2022-07-15 13:08:37 +0100

    Update Scard.tsx

--- src/components/testimonials/slider/Scard.tsx
+++ src/components/testimonials/slider/Scard.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 
 const Scard = (props:any) => {
-    const [troncate, settroncate] = useState(150)
+    const [troncate, settroncate] = useState(170)
      
   return (
-    <div style={{height: '80vh'}}>
+     
+ ​    ​<​div​ ​style​=​{​{​height​: ​'300px'​​}​}​>
           <img src={props.profile}  alt='person'/>
           <div className="myCarousel">
             <h3>{props.name}</h3>
             <h4>{props.proffesion}</h4>
-            <p onClick={()=> settroncate(props.desc.length === troncate ? 150 : props.desc.length)}>
-            {`${props.desc.slice(0, troncate )}${troncate === 150 &&'...'}`}
+            <p>
+            {`${props.desc.slice(0, troncate )}${troncate === 150 ? '...' : ''}`}
             </p>
           </div>
         </div>
