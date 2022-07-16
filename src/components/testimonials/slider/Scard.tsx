d54c31e5fac0468d64d0495006b178db738f8056
johnsmccain <johnsdanlami@gmail.com>
2022-07-16 03:34:50 +0100

    add just testimonial section

--- src/components/testimonials/slider/Scard.tsx
+++ src/components/testimonials/slider/Scard.tsx
@@ -4,13 +4,13 @@ const Scard = (props:any) => {
     const [troncate, settroncate] = useState(150)
      
   return (
-    <div style={{height: `${troncate > 150 ? '80vh': '286px'   }`}}>
+    <div style={{height: `${troncate > 200 ? '80vh': '300px'   }`}}>
           <img src={props.profile}  alt='person'/>
           <div className="myCarousel">
             <h3>{props.name}</h3>
             <h4>{props.proffesion}</h4>
-            <p onClick={()=> settroncate(props.desc.length === troncate ? 150 : props.desc.length)}>
-            {`${props.desc.slice(0, troncate )}${troncate === 150 &&'...'}`}
+            <p onClick={()=> settroncate(props.desc.length === troncate ? 200 : props.desc.length)}>
+            {`${props.desc.slice(0, troncate )}${troncate === 200 ?'...': ''}`}
             </p>
           </div>
         </div>
